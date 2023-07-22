fe64b8cae245e237434f1ddcb45573965e95d581
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-07-22 06:20:27 +0200

    chore: initial load

---
+++ cmd/sysdig.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	golang := client.Container().From("golang:1.20").WithExec([]string{"go", "version"})
+
+	version, err := golang.Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// print output
+	fmt.Println("Hello from Dagger and " + version)
+}
+
